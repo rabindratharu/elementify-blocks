bb2d59c421f01583b90c35bd67ebf34126fa5e6e
Rabindra Tharu <rabindra.tharu.np@gmail.com>
2024-05-08 17:21:51 +0545

    fixes

--- assets/build/editor/index.js
+++ assets/build/editor/index.js
@@ -1,1 +1,96 @@
-!function(){"use strict";var t;t=jQuery,new class{constructor(){this.authorProfileImgContainer=t("#author-profile-img span"),this.authorFirstNameText=t("#author-firstname").text(),this.authorLastNameText=t("#author-lastname").text(),this.init()}init(){if(!this.authorProfileImgContainer.length)return null;let t=this.authorFirstNameText.charAt(0)+this.authorLastNameText.charAt(0);t=t||"A",this.authorProfileImgContainer.text(t)}}}();
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./assets/src/editor/index.scss":
+/*!**************************************!*\
+  !*** ./assets/src/editor/index.scss ***!
+  \**************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+/*!************************************!*\
+  !*** ./assets/src/editor/index.js ***!
+  \************************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.scss */ "./assets/src/editor/index.scss");
+
+
+( function( $ ) {
+	class Author {
+		constructor() {
+			this.authorProfileImgContainer = $( '#author-profile-img span' );
+			this.authorFirstNameText = $( '#author-firstname' ).text();
+			this.authorLastNameText = $( '#author-lastname' ).text();
+			
+			this.init();
+		}
+		
+		init() {
+			if ( ! this.authorProfileImgContainer.length ) {
+				return null;
+			}
+			
+			let initials = this.authorFirstNameText.charAt( 0 ) + this.authorLastNameText.charAt( 0 );
+			initials = initials ? initials : 'A';
+			
+			// Set the text.
+			this.authorProfileImgContainer.text( initials );
+		}
+		
+	}
+	
+	new Author();
+} )( jQuery );
+
+}();
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
